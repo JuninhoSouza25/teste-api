a9f63bad83b39f32edffc41b44efdb1380278356
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-10-14 16:50:29 -0300

    novo commit

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-
-function Contador(){
-    const [contador, setContador] = useState(1)
-
-    function adicionarContador(){
-        setContador(contador + 1)
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Contador</button>
-        </div>
-    )
-}
 
 function Home(){
     return (
         <div>
             <div>Home</div>
-            <Contador />
         </div>
     )
 }
